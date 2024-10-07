77c7d6c45b6e518317cbe0d03419a276d2bd27c1
Misaka1932 <972488707@qq.com>
2024-10-07 19:37:55 +0800

    fixed

--- my_app/sendmail.py
+++ my_app/sendmail.py
@@ -2,7 +2,7 @@ from flask_mail import Message
 from flask import render_template
 import os
 import threading
-from my_app import app, mail
+from init import app, mail
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -16,3 +16,18 @@ def send_mail(subject, sender, recipients, text_body, html_body):
     thr1 = threading.Thread(target=send_async_email)
     thr1.start()
 
+def send_register_mail(recipients, username, token):
+    send_mail('口令验证',
+              sender=app.config['MAIL_USERNAME'],
+              recipients=[recipients],
+              text_body=render_template('../emails/register_mail.txt', user=username, token=token),
+              html_body=render_template('../emails/register_mail.html', user=username, token=token)
+             )
+
+def send_password_reset_email(token, recipients, username):
+    send_mail('重置密码',
+              sender=app.config['MAIL_USERNAME'],
+              recipients=[recipients],
+              text_body=render_template('../emails/reset_password.txt', user=username, token=token),
+              html_body=render_template('../emails/reset_password.html', user=username, token=token)
+             )
