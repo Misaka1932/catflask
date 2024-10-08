9ba5ec89c600babf6300389cbb5fd378cf73e83e
Misaka1932 <972488707@qq.com>
2024-10-08 12:48:50 +0800

    添加了404界面

--- my_app/main.py
+++ my_app/main.py
@@ -1,7 +1,7 @@
 import random
 from my_app import app
 from sendmail import send_register_mail, send_password_reset_email
-from flask import render_template, session, request, url_for, redirect
+from flask import render_template, session, request, url_for, redirect, abort
 from flask_mail import Message
 
 @app.route('/')
@@ -12,8 +12,6 @@ def empty_page():
 def home_page():
     return render_template('home.html')
 
-'''
-@app.route('/login')
-def login_page():
-    return render_template('login.html')
-'''
+@app.route('/404')
+def not_found_page():
+    return render_template('404.html'), 404
--- my_app/run.py
+++ my_app/run.py
@@ -1,2 +1,5 @@
 from my_app import app
+import webbrowser
+
+webbrowser.open_new('http://localhost:5000')
 app.run(debug=True)
